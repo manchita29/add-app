beab35b71a686f4caaaf0d3d9ccd5790b92c1c22
manchita29 <manchitasharma2005@gmail.com>
2024-12-04 20:57:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg'; 
+import './App.css';
+
+function App() {
+  const Name='Cipher!'
+  return (
+    <div>
+    <div>
+      <h1 style={{
+        color:"red",
+        textAlign:"center",
+      }}>hellow from {Name}</h1>
+    </div>
+   
+    <div>
+      <p>this is {Name}</p>
+    </div>
+    </div>
+  )
+}
+
+export default App;
